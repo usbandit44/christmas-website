003e5aa5e69af6d12c95c2e238796dbeed7be0ee
usbandit44 <usbandit44@gmail.com>
2023-12-10 21:51:08 -0700

    deploy setup

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import "./components/snowfall.scss";
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./list.css";
 import ListItem from "./ListItem";
 
